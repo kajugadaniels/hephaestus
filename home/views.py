6b2c1ae94d590c2b20a09071032a095972e11c31
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-23 20:01:33 +0200

    Retrieve users

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from account.models import *
 
 def dashboard(request):
     return render(request, 'pages/dashboard.html')
 
 def getUsers(request):
-    return render(request, 'pages/users/index.html')
+    getUsers = User.objects.all()
+
+    context = {
+        'getUsers': getUsers
+    }
+
+    return render(request, 'pages/users/index.html', context)
