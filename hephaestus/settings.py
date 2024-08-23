21d2af3f64ba025297c08a54d1206079c68161d7
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-23 11:20:11 +0200

    Deployment setup

--- hephaestus/settings.py
+++ hephaestus/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-7uflvye@b+7$5y7cw^kf(5yyvr-s9w!%kh4wy=87)7l0ln@$@h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -142,3 +142,5 @@ WHITENOISE_AUTOREFRESH = True
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://hephaestus.up.railway.app/','https://*.127.0.0.1']
