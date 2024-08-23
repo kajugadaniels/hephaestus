3bf98fffa7bfae08a6e9342facc4e8c3b71874a1
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-23 20:23:28 +0200

    Change routes

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 app_name = 'home'
 
 urlpatterns = [
-    path('', dashboard, name="dashboard"),
+    path('dashboard', dashboard, name="dashboard"),
 
     path('users/', getUsers, name="getUsers"),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
