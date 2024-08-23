6fa2e2138ad7511892208f9ccf30c8d66135650c
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-23 20:33:47 +0200

    Protect routes

--- home/views.py
+++ home/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
 from account.models import *
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def dashboard(request):
     return render(request, 'pages/dashboard.html')
 
+@login_required
 def getUsers(request):
-    getUsers = User.objects.all()
+    if request.user.role == 'Admin':
+        messages.error(request, "Vous n'êtes pas autorisé à accéder à cette page.")
+        return redirect('base:dashboard')
+
+    getUsers = User.objects.all().order_by('-created_at')
 
     context = {
         'getUsers': getUsers
