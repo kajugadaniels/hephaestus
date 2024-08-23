ee11c4cf5a563ca1402391d8939802d3a728200e
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-23 19:21:31 +0200

    Modify user model and add admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from account.models import *
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'email', 'phone_number', 'role', 'added_by', 'is_active', 'is_staff')
+    search_fields = ('name', 'email', 'phone_number')
+    prepopulated_fields = {'slug': ('name',)}
--- account/models.py
+++ account/models.py
@@ -1,7 +1,16 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+import os
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 from account.managers import *
+from django.utils import timezone
+from django.utils.text import slugify
+from imagekit.processors import ResizeToFill
+from imagekit.models import ProcessedImageField
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+def user_image_path(instance, filename):
+    base_filename, file_extension = os.path.splitext(filename)
+    return f'profile_images/user_{slugify(instance.name)}_{instance.pk}{file_extension}'
 
 class User(AbstractBaseUser, PermissionsMixin):
     ROLE_CHOICES = (
@@ -13,11 +22,21 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
     phone_number = models.CharField(max_length=15, unique=True)
-    image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+    image = ProcessedImageField(
+        upload_to=user_image_path,
+        processors=[ResizeToFill(720, 720)],
+        format='JPEG',
+        options={'quality': 90},
+        null=True,
+        blank=True,
+    )
     role = models.CharField(max_length=30, choices=ROLE_CHOICES)
-    
+
     added_by = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='added_users')
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -26,6 +45,17 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS = ['name', 'email']
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            existing_user = User.objects.get(pk=self.pk)
+            if not self.slug or self.name != existing_user.name:
+                self.slug = slugify(self.name)
+        else:
+            if not self.slug:
+                self.slug = slugify(self.name)
+                
+        super(User, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.email
