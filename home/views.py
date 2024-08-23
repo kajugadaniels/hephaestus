fe6efb17fea002d833b3178a792952aa42089bca
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-23 19:54:00 +0200

    Setup for retrieving users using datatables

--- home/views.py
+++ home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'pages/dashboard.html')
+
+def getUsers(request):
+    return render(request, 'pages/users/index.html')
