e371383f5d42b6ff346202c9b8705d89cf654b07
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-08-25 16:03:05 +0200

    Create Student model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,64 @@
+import os
+from datetime import date
 from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
-# Create your models here.
+def student_image_path(instance, filename):
+    base_filename, file_extension = os.path.splitext(filename)
+    return f'students/student_{slugify(instance.name)}_{instance.birthday}_{instance.gender}{file_extension}'
+
+class Student(models.Model):
+    GENDER_CHOICES = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+
+    name = models.CharField(max_length=255, null=True, blank=True)
+    slug = models.SlugField(unique=True, blank=True)
+    image = ProcessedImageField(
+        upload_to=student_image_path,
+        processors=[ResizeToFill(720, 720)],
+        format='JPEG',
+        options={'quality': 90},
+        null=True,
+        blank=True
+    )
+    dob = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=True)
+    nationality = models.CharField(max_length=40, null=True, blank=True)
+
+    # Contact Info
+    cell = models.CharField(max_length=50, null=True, blank=True)
+    village = models.CharField(max_length=50, null=True, blank=True)
+    sector = models.CharField(max_length=50, null=True, blank=True)
+    district = models.CharField(max_length=50, null=True, blank=True)
+
+    # Emergency Contact Information
+    emergency_contact_name = models.CharField(max_length=100, null=True, blank=True)
+    emergency_contact_relation = models.CharField(max_length=50, null=True, blank=True)
+    emergency_contact_phone = models.CharField(max_length=15, null=True, blank=True)
+
+    # Additional Information
+    medical_conditions = models.TextField(null=True, blank=True)
+    allergies = models.TextField(null=True, blank=True)
+    special_needs = models.TextField(null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def age(self):
+        today = date.today()
+        age = today.year - self.dob.year - ((today.month, today.day) < (self.dob.month, self.dob.day))
+        return age
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Student, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
