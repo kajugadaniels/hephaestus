ec92adc7042216135afa18a658fa0e576f672d88
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-17 16:14:36 +0200

    Modify full forms.py

--- home/forms.py
+++ home/forms.py
@@ -33,45 +33,15 @@ class UserCreationForm(forms.ModelForm):
 
         return cleaned_data
 
-class StudentForm(forms.ModelForm):
+class AcademicYearForm(forms.ModelForm):
     class Meta:
-        model = Student
-        fields = ['name', 'image', 'dob', 'gender', 'current_status', 'nationality',
-                  'cell', 'village', 'sector', 'district', 'emergency_contact_name',
-                  'emergency_contact_relation', 'emergency_contact_phone',
-                  'medical_conditions', 'allergies', 'special_needs', 'notes']
+        model = AcademicYear
+        fields = ['name', 'start_date', 'end_date']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
-            'image': forms.FileInput(attrs={'class': 'form-control'}),
-            'dob': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'required': 'true'}),
-            'gender': forms.Select(attrs={'class': 'form-control select2', 'required': 'true'}),
-            'current_status': forms.Select(attrs={'class': 'form-control select2', 'required': 'true'}),
-            'nationality': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
-            'cell': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
-            'village': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
-            'sector': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
-            'district': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
-            'emergency_contact_name': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
-            'emergency_contact_relation': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
-            'emergency_contact_phone': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
-            'medical_conditions': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Optional'}),
-            'allergies': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Optional'}),
-            'special_needs': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Optional'}),
-            'notes': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Optional'}),
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'required': 'true'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'required': 'true'}),
         }
-
-    # def __init__(self, *args, **kwargs):
-    #     super(StudentForm, self).__init__(*args, **kwargs)
-    #     self.fields['image'].required = False
-
-    # def clean_image(self):
-    #     image = self.cleaned_data.get('image')
-    #     if image:
-    #         if image.size > 5 * 1024 * 1024:  # 5MB limit
-    #             raise forms.ValidationError("Image file too large ( > 5MB )")
-    #         if image.content_type not in ['image/jpeg', 'image/png', 'image/webp']:
-    #             raise forms.ValidationError("Please upload a JPEG, PNG or WebP image.")
-    #     return image
 
 class TeacherForm(forms.ModelForm):
     class Meta:
@@ -118,37 +88,47 @@ class TeacherForm(forms.ModelForm):
             'achievements': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Optional'}),
         }
 
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['name', 'image', 'dob', 'gender', 'current_status', 'nationality',
+                  'cell', 'village', 'sector', 'district', 'emergency_contact_name',
+                  'emergency_contact_relation', 'emergency_contact_phone',
+                  'medical_conditions', 'allergies', 'special_needs', 'notes']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+            'dob': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'required': 'true'}),
+            'gender': forms.Select(attrs={'class': 'form-control select2', 'required': 'true'}),
+            'current_status': forms.Select(attrs={'class': 'form-control select2', 'required': 'true'}),
+            'nationality': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
+            'cell': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
+            'village': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
+            'sector': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
+            'district': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
+            'emergency_contact_name': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
+            'emergency_contact_relation': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
+            'emergency_contact_phone': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
+            'medical_conditions': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Optional'}),
+            'allergies': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Optional'}),
+            'special_needs': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Optional'}),
+            'notes': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Optional'}),
+        }
 
 class TermForm(forms.ModelForm):
     class Meta:
         model = Term
-        fields = ['academic_year', 'name', 'start_date', 'end_date']
+        fields = ['name', 'start_date', 'end_date']
         widgets = {
-            'academic_year': forms.Select(attrs={'class': 'form-control select2', 'required': 'true'}),
             'name': forms.Select(attrs={'class': 'form-control select2', 'required': 'true'}),
             'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'required': 'true'}),
             'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'required': 'true'}),
         }
 
-class AcademicYearForm(forms.ModelForm):
-    class Meta:
-        model = AcademicYear
-        fields = ['name', 'start_date', 'end_date']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
-            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'required': 'true'}),
-            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'required': 'true'}),
-        }
-
 class ClassForm(forms.ModelForm):
-    # students = forms.ModelMultipleChoiceField(
-    #     queryset=Student.objects.filter(delete_status=False),
-    #     required=False
-    # )
-
     class Meta:
         model = Class
-        fields = ['name', 'grade', 'section', 'head_teacher', 'students', 'capacity', 'academic_year']
+        fields = ['name', 'grade', 'section', 'head_teacher', 'students', 'capacity']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'required': 'true'}),
             'grade': forms.Select(attrs={'class': 'form-control', 'required': 'true'}),
@@ -158,7 +138,6 @@ class ClassForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ClassForm, self).__init__(*args, **kwargs)
         self.fields['head_teacher'].queryset = Teacher.objects.filter(delete_status=False)
-        self.fields['academic_year'].queryset = AcademicYear.objects.filter(delete_status=False)
 
 class SubjectForm(forms.ModelForm):
     class Meta:
@@ -166,7 +145,7 @@ class SubjectForm(forms.ModelForm):
         fields = ['name', 'description']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
 class ClassSubjectForm(forms.ModelForm):
